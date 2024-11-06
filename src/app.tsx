eff4e5b7639dc6cc407f274041376e860434c302
PdroFreitass <pedrothdvzs@gmail.com>
2024-11-05 23:35:38 -0300

    projectreacttailwind

---
+++ src/app.tsx
@@ -0,0 +1,15 @@
+export function App() {
+  return (
+    <div className="">
+      <p>Convide seus amigos e planeje sua proxima viagem!</p>
+      <div>
+        
+      </div>
+      <p>Ao planejar sua viagem pela plann.er voce automaticamente concorda
+        com nossos <a href="#">termos de uso</a> e <a href="#">politicas de privacidade.</a>
+      </p>
+    </div>
+  )
+}
+
+
